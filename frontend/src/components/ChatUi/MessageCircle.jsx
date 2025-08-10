392eb5946fca56b8b4264bef69860eac66649bbf
Supun Hashintha <hashinthaliyanaarchchi373@gmail.com>
2025-08-10 21:39:58 +0530

    new commit

--- frontend/src/components/ChatUi/MessageCircle.jsx
+++ frontend/src/components/ChatUi/MessageCircle.jsx
@@ -8,7 +8,13 @@ const MessageCircle = () => {
         {id: 1 , text : 'Hello! How can i help you today? , sender , bot'}
     ]);
   return (
-    <div>MessageCircle</div>
+    <div>
+      <div className="message-circle">
+        <div className="message-circle__header">
+          <div className="message-circle__header__title">Chat</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
