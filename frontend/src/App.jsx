f48c72d7346ed6c56aacb629f33c387d69044f7e
Supun Hashintha <hashinthaliyanaarchchi373@gmail.com>
2025-08-12 10:02:28 +0530

    new comit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <AppLayout />,
+      element: <AppLayout/>,
       children: [
         {
           index: true,
